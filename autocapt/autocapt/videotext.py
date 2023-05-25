4ee18fddfee404b0176261012cdb980a4948caa1
jackgutheim <jack.gutheim@quantfive.org>
2023-05-25 11:50:20 -0700

    using relative positioning

--- autocapt/autocapt/videotext.py
+++ autocapt/autocapt/videotext.py
@@ -8,10 +8,13 @@ from moviepy.editor import *
 import whisper_timestamped as whisper
 
 # Change this based on which video you would like to process
-video_name = "autocapt/Michael_Owen"
+video_name = "autocapt/macdemarco"
 
 # How many words you want per caption section
-caption_grouping = 3
+caption_grouping = 4
+
+# Font Size
+font_size = 50
 
 type = "mp3"
 filename, ext = os.path.splitext(video_name + ".mp4")
@@ -45,12 +48,12 @@ for i in range(len(segments)):
         for x in range(last_few if last_run else caption_grouping):
                 text_string = text_string + str(words[j + x]["text"]) + " "
 
-        txt_clip = TextClip(text_string, fontsize=50, color="red", align='center')
+        txt_clip = TextClip(text_string, fontsize=font_size, color="red")
         txt_clip = (
-            txt_clip.set_position((0.35,0.8), relative=True)
+            txt_clip
             .set_start(words[j]["start"])
             .set_duration(words[j + ((last_few if last_run else caption_grouping) - 1)]["end"] - words[j]["start"])
-        )
+        ).set_position(("center", "bottom"))
         txt_clips.append(txt_clip)
         if (j + caption_grouping) <= num_words:
             j += caption_grouping
